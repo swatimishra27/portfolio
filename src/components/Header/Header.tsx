d5968320ecadefd47c5af548710b952774f70ddb
swatimishra27 <swatimishra7588@gmail.com>
2026-02-25 12:34:24 +0530

    update package dependencies and add new type definitions

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 import { Container } from './styles'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
